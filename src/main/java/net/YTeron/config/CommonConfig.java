c71de194d1d9c4ff4484e4f8bddd6f0c7b1688d6
YTeron <artem.trusov08@mail.ru>
2026-06-08 10:52:09 +0300

    1 save

---
+++ src/main/java/net/YTeron/config/CommonConfig.java
@@ -0,0 +1,16 @@
+package net.YTeron.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class CommonConfig {
+    public static final ForgeConfigSpec.Builder BUILDER =new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SPEC;
+
+    static{
+        BUILDER.push("Config for TUTS");
+
+
+        BUILDER.pop();
+        SPEC = BUILDER.build();
+    }
+}
